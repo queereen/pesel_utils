a7d7f86443f93b836d783909a79225b7b75e6fe9
Avis Drożniak <queereen@riseup.net>
2021-04-17 17:26:47 +0200

    Fixes.
    Signed-off-by: Avis Drożniak <queereen@riseup.net>

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pesel_utils',
     version='1.0',
     description='Utilities for checking PESEL numbers',
+    long_description=open('README.md').read(),
     license='Hippocratic License 2.1',
     url='https://github.com/queereen/pesel_utils',
     author='Avis Drożniak',
     author_email='queereen@riseup.net',
-    packages=['pesel_utils'],
+    packages=find_packages(),
     install_requires=['numpy']
     )
--- src/pesel_utils/__init__.py
+++ src/pesel_utils/__init__.py
@@ -1,1 +1,48 @@
-import numpy 
+from numpy import multiply
+
+def is_valid(pesel):
+    p = list(map(int, [char for char in pesel]))
+    w = [1,3,7,9,1,3,7,9,1,3,1]
+    if (len(p) == 11):
+        checksum = list(map(int, [char for char in str(sum(multiply(p,w)))]))
+        if(checksum[-1] == 0):
+            return True
+        else:
+            return False 
+    else:
+        return False
+
+def check_gender(pesel):
+    p = list(map(int, [char for char in pesel]))
+    if (is_valid(p)==False):
+        return 'invalid PESEL'
+    else:
+        if(p[9]%2==0):
+            return 'female'
+        else:
+            return 'male'
+
+def check_birthdate(pesel):
+   p = list(map(int, [char for char in pesel]))
+   if (is_valid(p)==False):
+       return 'invalid PESEL'
+   else:
+       year=str(p[0])+str(p[1])
+       month=int(str(p[2])+str(p[3]))
+       day=str(p[4])+str(p[5])
+       if (month >= 81 and month <= 92):
+           year=str(18)+year
+           month-=80
+       elif (month >= 1 and month <= 12):
+           year=str(19)+year
+       elif (month >= 21 and month <= 32):
+           year=str(20)+year
+           month-=20
+       elif (month >= 41 and month <= 52):
+           year=str(21)+year
+           month-=40
+       elif (month >= 61 and month <= 72):
+           year=str(22)+year
+           month-=60
+       
+       return(str(year)+"/"+str(month)+"/"+str(day))
