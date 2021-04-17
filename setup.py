3603bc2ddbb6c31f9347a4c4363ec5a67036c323
Avis Drożniak <queereen@riseup.net>
2021-04-17 17:02:48 +0200

    Initial commit.
    Signed-off-by: Avis Drożniak <queereen@riseup.net>

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pesel_utils',
+    version='1.0',
+    description='Utilities for checking PESEL numbers',
+    license='Hippocratic License 2.1',
+    url='https://github.com/queereen/pesel_utils',
+    author='Avis Drożniak',
+    author_email='queereen@riseup.net',
+    packages=['pesel_utils'],
+    install_requires=['numpy']
+    )
